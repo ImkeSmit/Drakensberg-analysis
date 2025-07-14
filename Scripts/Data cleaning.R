7a2750c27eab516fd92263c84338eb1e5947205a
ImkeSmit <imke610@gmail.com>
2025-07-14 10:41:29 +0200

    create a function to change the species names

--- Scripts/Data cleaning.R
+++ Scripts/Data cleaning.R
@@ -119,8 +119,8 @@ test <- gg_summer[grep("asclep", gg_summer$taxon), ]
 gg_summer[grep("gladiolus", gg_summer$taxon), ]
 
 
-####Import trait data####
-#for now, we'll just get the names from the trait data, and clean the rest later
+####get names from trait data####
+#for now, we'll just get the names from the trait data, and clean the rest of the FT data later
 
 gg_trait_names <- read_excel("All_data/raw_trait_data/GG_dataset_Functional_traits.xlsx") |> 
   rename(taxon = Species) |> 
@@ -157,3 +157,50 @@ all_trait_sp <- gg_trait_names |>
 #create a trait names file
 write.xlsx(all_trait_sp, "All_data/clean_data/micro_climb_trait_names.xlsx", sheetName = "trait_names", overwrite = F)
 
+
+
+###Now clean and standardise names####
+#create function to change incorrect names
+standardise_names <- function(data, #dataframe containing species names that need to be corrected
+                                   data_species_column, #column in data contain the speciesnames, must be a string
+                                   #species names must be in the format genus_species.
+                                   #I.e. no capitals, with an underscore separating genus and specific epithet
+                                   naming_system,  #dataframe containing the old names that need to be changed, and the names they should be changed to
+                                   correct_name,  #column in naming_system that has the correct name
+                                   synonym) #columns in naming_system that has the synonyms
+{
+  
+  for (i in 1:nrow(data)) {
+    old_name <- data[i, which(colnames(data) == data_species_column)]
+    new_name <- NA
+    
+    found <- FALSE
+    for (j in 1:nrow(naming_system)) { # looks whether species name should be corrected and replaces it with the new_heli_name_system in case
+      found <- grepl(old_name, naming_system[j, which(colnames(naming_system) %in% synonym)])
+      
+      if (is.na(found)){ # only runs if the species is missing
+        found <- FALSE
+      }
+      
+      if (TRUE %in% found){ # only runs if the species is a synonym
+        new_name <- naming_system[j, which(colnames(naming_system) %in% correct_name)] # finds the true name of the species and saves it
+        break
+      }
+    }
+    
+    if (found) { # replaces the species in the trait database with the saved true name if "found" is "TRUE"
+      data[i, which(colnames(data) == data_species_column)] <- new_name
+      
+      #add column to keep track of which names changed
+      data <- data |> 
+        mutate(change_tracker = paste0("row", "=", i, " ", "name changed from ", old_name, " to ", new_name))
+    }
+  }#end loop through rows
+  return(data)
+}#end function
+
+
+#import naming system 
+name_trail <- read.xlsx("All_data/clean_data/micro_climb_ALL_names_editing.xlsx", sheet = "editing")
+
+clean_names_WH <- standardise_names(wh, "taxon", naming_system = name_trail, "taxon", c("synonym1", "synonym2", "synonym3"))
