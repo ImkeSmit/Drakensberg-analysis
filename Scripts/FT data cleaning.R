2bce29d1be03fd54bcc24a6d6197d09ce00b0176
ImkeSmit <imke610@gmail.com>
2025-07-22 13:48:48 +0200

    use the new name trail

--- Scripts/FT data cleaning.R
+++ Scripts/FT data cleaning.R
@@ -124,7 +124,7 @@ GG <- GG |>
 
 
 #standardise names
-name_trail <- read.xlsx("All_data/clean_data/Species names/micro_climb_ALL_names_editing - Copy.xlsx", sheet = "editing")
+name_trail <- read.xlsx("All_data/clean_data/Species names/micro_climb_ALL_names_editing.xlsx", sheet = "editing")
 GG_clean_names <- standardise_names(data = GG, data_species_column = "Taxon", 
                                       naming_system = name_trail, correct_name = "taxon", 
                                     synonym = c("synonym1", "synonym2", "synonym3"))
