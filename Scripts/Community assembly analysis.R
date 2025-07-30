1ac6987c309e1c9d2e111ddb6b732562ffb08f64
ImkeSmit <imke610@gmail.com>
2025-07-30 12:42:01 +0200

    try some things

--- Scripts/Community assembly analysis.R
+++ Scripts/Community assembly analysis.R
@@ -1,3 +1,12 @@
 ##Commmunity assembly analysis####
 install.packages("untb")
 library(untb)
+Sys.setenv(gp_binary = "C:/Program Files/Pari64-2-17-2/gp.exe")
+gp_binary = "C:/Program Files/Pari64-2-17-2/gp.exe"
+
+data("saunders")
+summary(saunders.tot)
+preston(saunders.tot, n= 9)
+optimal.theta(saunders.tot)
+optimal.params(saunders.tot, gp_binary = "C:/Program Files/Pari64-2-17-2/gp.exe")
+etienne(saunders.tot)
