e09c2d5572fba1a69f201f323a21f18a6d3581d9
ImkeSmit <imke610@gmail.com>
2025-09-10 09:24:12 +0200

    calculate trait coverage

--- Scripts/trait coverage.R
+++ Scripts/trait coverage.R
@@ -13,4 +13,16 @@ occ <- read.xlsx("All_data/clean_data/micro_climb_occurrence.xlsx")
 comb <- occ |>
   left_join(FT, by = c("cellref", "taxon"))
 
-#
+#how many sp in occ do we have at least one trait value for?
+nsp_occ <- occ |> 
+  distinct(taxon) |> 
+  summarise(n = n()) #414 total sp in occ
+
+nsp_traits <- comb |> 
+  filter(!is.na(Sample_ID)) |> #keep only sp for which we have traits
+  distinct(taxon) |> 
+  summarise(n = n())
+
+p_traits <- nsp_traits/nsp_occ *100 #we have at least one trait val for 35% of species
+
+
