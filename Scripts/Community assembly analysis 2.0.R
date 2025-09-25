c5526ade7c3076930c90b4f8af18f6f878ba9e06
ImkeSmit <imke610@gmail.com>
2025-09-25 13:33:25 +0200

    do some trait filling

--- Scripts/Community assembly analysis 2.0.R
+++ Scripts/Community assembly analysis 2.0.R
@@ -4,12 +4,15 @@ library(tidyverse)
 library(tidylog)
 library(ggplot2)
 library(vegan)
+library(traitstrap)
 
 drak <- read.xlsx("All_data/clean_data/micro_climb_occurrence.xlsx") |> 
   mutate(cell = paste0(column, row))
 FT <- read.xlsx("All_data/clean_data/micro-climb_traits.xlsx") |> 
   rename(taxon = Taxon, 
-         site = Site, grid = Grid, cell = Cell) 
+         site = Site, grid = Grid, cell = Cell) |> 
+  pivot_longer(cols = c(Wet_mass_mg, Dry_mass_mg, Chlorophyll_mg_per_m2, Ft, Height_cm, 
+                        Thickness_mm, Leaf_area_mm2, SLA, LDMC), names_to = "trait", values_to = "value")
   
 
 #combine occurrence and trait data
@@ -19,6 +22,19 @@ comb <- drak |>
   select(!c(column, row))
 
 #Trait filling###
+comb <- trait_fill(
+  comm = drak,
+  traits = FT,
+  scale_hierarchy = c("site", "grid"),
+  global = F,
+  taxon_col = "taxon",
+  trait_col = "trait",
+  value_col = "value",
+  abundance_col = "cover",
+  keep_all = FALSE,
+  min_n_in_sample = 5,
+  complete_only = FALSE
+)
 
 
 
