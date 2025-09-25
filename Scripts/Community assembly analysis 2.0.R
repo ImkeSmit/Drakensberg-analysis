f1435eb488314b5719f9f01dfa78c5cc82a84d18
ImkeSmit <imke610@gmail.com>
2025-09-25 13:16:58 +0200

    import and join occurrence and trait data

--- Scripts/Community assembly analysis 2.0.R
+++ Scripts/Community assembly analysis 2.0.R
@@ -3,16 +3,22 @@ library(openxlsx)
 library(tidyverse)
 library(tidylog)
 library(ggplot2)
-library(sads)
 library(vegan)
 
-drak <- read.xlsx("All_data/clean_data/micro_climb_occurrence.xlsx")
+drak <- read.xlsx("All_data/clean_data/micro_climb_occurrence.xlsx") |> 
+  mutate(cell = paste0(column, row))
+FT <- read.xlsx("All_data/clean_data/micro-climb_traits.xlsx") |> 
+  rename(taxon = Taxon, 
+         site = Site, grid = Grid, cell = Cell) 
+  
 
-ab <- drak |> 
-  group_by(taxon) |> 
-  summarise(abundance = n(), 
-            total_cover = sum(cover)) |> 
-  ungroup()
-ab$abundance <- as.numeric(ab$abundance)
-ab <- as.data.frame(ab)
-ab <- ab[order(ab$abundance, decreasing = TRUE), ]
+#combine occurrence and trait data
+comb <- drak |> 
+  full_join(FT, by = c("site", "grid", "cell", "taxon")) |> #do full join so that we can trait fill
+  mutate(cellref = paste0(site, grid, cell)) |> 
+  select(!c(column, row))
+
+#Trait filling###
+
+
+
