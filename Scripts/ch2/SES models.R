ad27651af49ebcd2dcc10a1997c16919d63ed3e1
ImkeSmit <imke610@gmail.com>
2025-10-21 13:16:24 +0200

    try to model with a gam

--- Scripts/ch2/SES models.R
+++ Scripts/ch2/SES models.R
@@ -11,7 +11,7 @@ cell_ses <- read.csv("All_data/comm_assembly_results/RQ_cells_C5_entire.csv", ro
   mutate(elevation = case_when(grepl("BK", cellref) == T ~ "3000", #add elevation variable
                                grepl("WH", cellref) == T ~ "2500",
                                grepl("GG", cellref) == T ~ "2000",.default = NA))
-  
+cell_ses$elevation <- as.factor(cell_ses$elevation)  
 
 
 
@@ -25,8 +25,18 @@ ses_ridges <- cell_ses |>
 #models of ses~ elevation
 hist(cell_ses$SES)
 modeldat <- cell_ses[which(cell_ses$trait == "Height_cm"), ]
-test <- lm(SES ~ elevation_char, data = modeldat)
+test <- lm(SES ~ elevation, data = modeldat)
 summary(test)
+anova(test)
+
+#how badly are the assumptions violated?
+plot(test)#ja very badly
+
+#Let's try a GAM
+#USe skew t distribution as recommended by chatgpt.Can handle skewed and heavy tailed data with nonzero values
+test2 <- gamlss(SES ~ elevation, data = modeldat, family = ST1())
+summary(test2)
+plot(test2) #looks much better
 
 
 ###Grid scale####
