40a4398b6ad50725379ff57ad131093118af7c5f
ImkeSmit <imke610@gmail.com>
2025-10-27 09:08:22 +0200

    try to figure out reason for high SES

--- Scripts/ch2/SES models.R
+++ Scripts/ch2/SES models.R
@@ -40,10 +40,63 @@ plot(test2) #looks much better
 
 
 ###What is going on in the cells with high SES values?
-q <- cell_ses |> 
+high <- cell_ses |> 
   slice_max(SES, n = 20)
 
-plotspecnumber(abun_matrix)[]
+high_plots <- cell_ses |> 
+  slice_max(SES, n = 20) |> 
+  distinct(cellref)
+
+#occurrence data
+drak <- read.xlsx("All_data/clean_data/micro_climb_occurrence.xlsx") |> 
+  mutate(cell = paste0(column, row))
+
+#trait data
+FT <- read.xlsx("All_data/clean_data/micro-climb_traits.xlsx") |> 
+  rename(taxon = Taxon, 
+         site = Site, grid = Grid, cell = Cell) |> 
+  pivot_longer(cols = c(Wet_mass_mg, Dry_mass_mg, Chlorophyll_mg_per_m2, Ft, Height_cm, 
+                        Thickness_mm, Leaf_area_mm2, SLA, LDMC), names_to = "trait", values_to = "value")
+
+#combine occurrence and trait data
+FT_join <- drak |> 
+  inner_join(FT, by = c("site", "grid", "cell", "taxon")) |> #inner join to only work with taxa that have trait data
+  mutate(cellref = paste0(site, grid, cell)) |> 
+  select(!c(column, row))
+
+#create abundance matrix
+abun_matrix <- FT_join |> 
+  select(cellref, taxon, cover) |> 
+  distinct(cellref, taxon, .keep_all = T) |> 
+  ungroup() |> 
+  arrange(taxon) |> 
+  mutate(cover = ceiling(cover)) |> #change cover values to integer to use in null models
+  pivot_wider(names_from = taxon, values_from = cover) 
+
+abun_matrix <- as.data.frame(abun_matrix)
+row.names(abun_matrix) <- abun_matrix$cellref
+abun_matrix <- abun_matrix[, -1]
+
+#replace NA values with 0
+for(r in 1:nrow(abun_matrix)) {
+  for(c in 1:ncol(abun_matrix)) {
+    
+    if(is.na(abun_matrix[r,c])) {
+      abun_matrix[r,c] <- 0
+    }
+  }
+}
+
+high_mat <- abun_matrix[which(row.names(abun_matrix) %in% c(high_plots$cellref)) , ]
+specnumber(high_mat) #all have between 2 and 6 species
+
+all_abun <- as.data.frame(specnumber(abun_matrix))
+all_abun$cellref <- row.names(all_abun)
+row.names(all_abun) <- NULL
+
+cell_ses2 <- cell_ses |> 
+  left_join(all_abun, by = "cellref")
+
 
 #GG4F18 has the highest SES, look at it's diversity
 s1 <- abun_matrix[which(rownames(abun_matrix) == "GG4F18") ,, drop = F]
