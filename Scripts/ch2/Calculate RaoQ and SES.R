1955d1f21a4d3ae0235c1a95b8335e8d018ede35
ImkeSmit <imke610@gmail.com>
2025-10-27 10:12:35 +0200

    calculate SES with the scaled RaoQ

--- Scripts/ch2/Calculate RaoQ and SES.R
+++ Scripts/ch2/Calculate RaoQ and SES.R
@@ -416,7 +416,7 @@ set.seed(123)
 nullcomm_cells <- generate_C5_null_imp(abun_matrix, 999, pool = "entire")
 saveRDS(nullcomm_cells, file = "All_data/comm_assembly_results/nullmodel_C5_cells.rds")
 
-#Calculate SES#
+#Calculate SES, with unscaled RaoQ#
 #we need to calculate RaoQ for each of the observed null communities
 
 # Set up parallel backend
@@ -435,6 +435,52 @@ for (i in seq_along(chunks)) { #run chunks sequentially
   sub_RQ <- future_lapply(seq_along(chunk_list), function(idx) {
     chosen_null <- chunk_list[[idx]]
     RQ_result <- calc_RaoQ(mean_traits, chosen_null)
+    RQ_result$counter <- paste0("null matrix ", (i - 1) * 100 + idx)
+    RQ_result
+  }, future.seed = TRUE) #generates a unique reproducible sub seed for each worker
+  #ensures that results are reproducible, and that there is no overlap in random processes for each core
+  
+  RaoQ_results[[i]] <- bind_rows(sub_RQ) #results from the chunk are merged
+  rm(sub_RQ, chunk_list); gc()
+}
+
+null_RQ <- bind_rows(RaoQ_results)
+
+plan(sequential)
+
+
+#SES of each cell
+RQ_cells_summary <- null_RQ |> 
+  group_by(trait, cellref) |> 
+  summarise(sd_null = sd(RaoQ), 
+            mean_null = mean(RaoQ)) |> 
+  filter(sd_null > 0) |> #cannot divide by zero in SES calculation
+  inner_join(RQ_obs_cells, by = c("trait", "cellref")) |> 
+  mutate(SES = (RaoQ - mean_null)/sd_null)
+
+write.csv(RQ_cells_summary, "All_data/comm_assembly_results/RQ_cells_C5_entire.csv")
+
+
+
+#Calculate SES, with scaled RaoQ#
+#we need to calculate RaoQ for each of the observed null communities
+
+# Set up parallel backend
+plan(multisession, workers = parallel::detectCores() - 2)  # or plan(multicore) on Linux/mac
+
+chunks <- split(nullcomm_cells, ceiling(seq_along(nullcomm_cells) / 50))
+#each chunk has 50 null matrices
+RaoQ_results <- list()
+
+for (i in seq_along(chunks)) { #run chunks sequentially
+  message("Processing chunk ", i, " of ", length(chunks))
+  
+  chunk_list <- chunks[[i]]  # smaller subset only
+  
+  #each core receives one null matrix to run calc_RaoQ on. this happens until all matrices in the chunk are finished
+  sub_RQ <- future_lapply(seq_along(chunk_list), function(idx) {
+    chosen_null <- chunk_list[[idx]]
+    RQ_result <- calc_RaoQ_scale(mean_traits, chosen_null)
     RQ_result$counter <- paste0("null matrix ", (i - 1) * 100 + idx)
     RQ_result
   }, future.seed = TRUE) #generates a unique reproducible sub seed for each worker
