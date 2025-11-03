0c1e8fe34ca64edb073e9b5a1b9a75380ba7da36
ImkeSmit <imke610@gmail.com>
2025-11-03 12:47:13 +0200

    delete the test code

--- Scripts/ch2/Create abundance and trait matrix.R
+++ Scripts/ch2/Create abundance and trait matrix.R
@@ -26,11 +26,7 @@ mean_traits <- FT |>
   ungroup() |> 
   arrange(taxon)
 
-mean_traits <- as.data.frame(mean_traits)
-row.names(mean_traits) <- mean_traits$taxon
-mean_traits <- mean_traits[, -1]
-
-#Do inner join between trait and cover data to get sp that match ebtween the two
+#Do inner join between trait and cover data to get sp that match between the two
 FT_join <- drak |> 
   inner_join(mean_traits, by = "taxon") |> #inner join to only work with taxa that have trait data
   mutate(cellref = paste0(site, grid, cell)) |> 
@@ -73,45 +69,3 @@ row.names(mean_traits) <- mean_traits$taxon
 mean_traits <- mean_traits[, -1]
 
 write.csv(mean_traits, "All_data/comm_assembly_results/mean_traits.csv")
-
-
-mean_traits2 <- FT |> 
-  filter(trait %in% c("Height_cm", "Leaf_area_mm2","Thickness_mm", "SLA", "LDMC")) |> 
-  group_by(taxon, trait) |> 
-  summarise(mean_trait = mean(value, na.rm = T)) |> 
-  pivot_wider(names_from = trait, values_from = mean_trait) |> 
-  ungroup() |> 
-  arrange(taxon)
-
-FT_join2 <- drak |> 
-  inner_join(mean_traits2, by = "taxon") |> #inner join to only work with taxa that have trait data
-  mutate(cellref = paste0(site, grid, cell)) |> 
-  select(!c(column, row))
-
-#create abundance matrix
-abun_matrix2 <- FT_join2 |> 
-  select(cellref, taxon, cover) |> 
-  distinct(cellref, taxon, .keep_all = T) |> 
-  ungroup() |> 
-  arrange(taxon) |> 
-  mutate(cover = ceiling(cover)) |> #change cover values to integer to use in null models
-  pivot_wider(names_from = taxon, values_from = cover) 
-
-abun_matrix2 <- as.data.frame(abun_matrix2)
-row.names(abun_matrix2) <- abun_matrix2$cellref
-abun_matrix2 <- abun_matrix2[, -1]
-
-#replace NA values with 0
-for(r in 1:nrow(abun_matrix2)) {
-  for(c in 1:ncol(abun_matrix2)) {
-    
-    if(is.na(abun_matrix2[r,c])) {
-      abun_matrix2[r,c] <- 0
-    }
-  }
-}
-
-#remove sites that have only one species 
-no <- specnumber(abun_matrix2)
-abun_matrix2 <- abun_matrix2[-which(no == 1), ]
-
