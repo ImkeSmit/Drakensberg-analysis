fe10c29fcea3d30238fb7d2ef5658e0d6b5f4f29
ImkeSmit <imke610@gmail.com>
2025-11-03 13:01:27 +0200

    import this data already processed

--- Scripts/ch2/Calculate RaoQ and SES.R
+++ Scripts/ch2/Calculate RaoQ and SES.R
@@ -12,77 +12,9 @@ library(future.apply)
 #load required functions first
 
 ####Import community and trait data####
-#occurrence data
-drak <- read.xlsx("All_data/clean_data/micro_climb_occurrence.xlsx") |> 
-  mutate(cell = paste0(column, row))
+abun_matrix <-read.csv("All_data/comm_assembly_results/abun_matrix.csv", row.names = 1)
 
-#trait data
-FT <- read.xlsx("All_data/clean_data/micro-climb_traits.xlsx") |> 
-  rename(taxon = Taxon, 
-         site = Site, grid = Grid, cell = Cell) |> 
-  pivot_longer(cols = c(Wet_mass_mg, Dry_mass_mg, Chlorophyll_mg_per_m2, Ft, Height_cm, 
-                        Thickness_mm, Leaf_area_mm2, SLA, LDMC), names_to = "trait", values_to = "value")
-  
-#combine occurrence and trait data
-FT_join <- drak |> 
-  inner_join(FT, by = c("site", "grid", "cell", "taxon")) |> #inner join to only work with taxa that have trait data
-  mutate(cellref = paste0(site, grid, cell)) |> 
-  select(!c(column, row))
-
-#Trait filling, leave for now
-#comb <- trait_fill(
-#  comm = drak,
-#  traits = FT,
-#  scale_hierarchy = c("site", "grid"),
-#  global = F,
-#  taxon_col = "taxon",
-#  trait_col = "trait",
-#  value_col = "value",
-#  abundance_col = "cover",
-#  keep_all = FALSE,
-#  min_n_in_sample = 5,
-#  complete_only = FALSE
-#)
-
-#Get mean traits for species
-mean_traits <- FT_join |> 
-  filter(trait %in% c("Height_cm", "Leaf_area_mm2", "SLA", "LDMC")) |> 
-  group_by(taxon, trait) |> 
-  summarise(mean_trait = mean(value, na.rm = T)) |> 
-  pivot_wider(names_from = trait, values_from = mean_trait) |> 
-  ungroup() |> 
-  arrange(taxon)
-
-mean_traits <- as.data.frame(mean_traits)
-row.names(mean_traits) <- mean_traits$taxon
-mean_traits <- mean_traits[, -1]
-
-#create abundance matrix
-abun_matrix <- FT_join |> 
-  select(cellref, taxon, cover) |> 
-  distinct(cellref, taxon, .keep_all = T) |> 
-  ungroup() |> 
-  arrange(taxon) |> 
-  mutate(cover = ceiling(cover)) |> #change cover values to integer to use in null models
-  pivot_wider(names_from = taxon, values_from = cover) 
-
-abun_matrix <- as.data.frame(abun_matrix)
-row.names(abun_matrix) <- abun_matrix$cellref
-abun_matrix <- abun_matrix[, -1]
-
-#replace NA values with 0
-for(r in 1:nrow(abun_matrix)) {
-  for(c in 1:ncol(abun_matrix)) {
-    
-    if(is.na(abun_matrix[r,c])) {
-      abun_matrix[r,c] <- 0
-    }
-  }
-}
-
-#remove sites that have only one species 
-no <- specnumber(abun_matrix)
-abun_matrix <- abun_matrix[-which(no == 1), ]
+mean_traits <- read.csv("All_data/comm_assembly_results/mean_traits.csv", row.names = 1)
 
 ####SES at cell scale, C5, pool = entire, unweighted RaoQ####
 #observed RaoQ, not weighted by abundance
