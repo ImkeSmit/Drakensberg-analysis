5918558f468afdb372e1ff08ededef36ef3a6afa
ImkeSmit <imke610@gmail.com>
2025-11-06 10:45:43 +0200

    save the RQ results from the C2 null model

--- Scripts/ch2/3.1 Calculate RaoQ and SES -C2.R
+++ Scripts/ch2/3.1 Calculate RaoQ and SES -C2.R
@@ -70,7 +70,7 @@ RQ_cells_summary <- null_RQ |>
   inner_join(RQ_obs_cells, by = c("trait", "cellref")) |> 
   mutate(SES = (RaoQ - mean_null)/sd_null)
 
-write.csv(RQ_cells_summary, "All_data/comm_assembly_results/RQ_weighted_cells_C5_entire.csv")
+write.csv(RQ_cells_summary, "All_data/comm_assembly_results/RQ_weighted_cells_C2_entire.csv")
 
 
 ####Forbs only - SES at cell scale, C5, pool = entire, weighted RaoQ####
