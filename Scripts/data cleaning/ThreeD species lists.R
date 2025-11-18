137800a3c4854ec194a48a2f90e764b4724bfc1a
ImkeSmit <imke610@gmail.com>
2025-11-18 09:10:04 +0200

    create a script that creates species lists of the threed experiment

---
+++ Scripts/data cleaning/ThreeD species lists.R
@@ -0,0 +1,16 @@
+###Script to create Three-D species lists
+library(tidyverse)
+library(openxlsx)
+
+#import clean data
+comm <- read.xlsx("All_data/clean_data/threed/community_2025.xlsx")
+
+plotsp <- comm |> 
+  group_by(destSiteID, origBlockID, turfID) |> 
+  distinct(Species) |> 
+  mutate(destplotID = as.numeric(str_split_i(turfID, "_", 3)), 
+         survey_date = "March 2025") |> 
+  arrange(destplotID) |> 
+  select(survey_date, destSiteID, origBlockID, destplotID, turfID, Species)
+
+write.xlsx(plotsp, "All_data/clean_data/threed/plot_composition_2025.xlsx")
