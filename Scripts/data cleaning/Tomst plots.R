13e032365f4b924987f26f3e5e939827730fbefe
ImkeSmit <imke610@gmail.com>
2026-02-12 10:40:02 +0200

    create plots for moisture and temperature for one logger

--- Scripts/data cleaning/Tomst plots.R
+++ Scripts/data cleaning/Tomst plots.R
@@ -1,4 +1,30 @@
-###Create timesereies plots of each Tomst logger
+###Create timeseries plots of each Tomst logger
 
 library(ggplot)
 library(ggpubr)
+
+#import data
+GG <- read.table("All_data/clean_data/Tomst_data/GoldenGate_tomst_data.txt", sep = "\t")
+
+TMS<- unique(GG$TMS)
+one_logger <- GG |> 
+  filter(TMS == TMS[1])
+
+soil_temp<-   ggplot(one_logger, aes(x = as.Date(datetime), y = soil_temp)) +
+  geom_line(col = "black") +
+  theme_classic()
+
+surface_temp <- ggplot(one_logger, aes(x = as.Date(datetime), y = surface_temp)) +
+  geom_line(col = "darkgreen") +
+  theme_classic()
+
+air_temp <- ggplot(one_logger, aes(x = as.Date(datetime), y = air_temp)) +
+  geom_line( col = "blue") +
+  theme_classic()
+
+soil_moist <- ggplot(one_logger, aes(x = as.Date(datetime), y = raw_moisture)) +
+  geom_line( col = "red") +
+  theme_classic()
+
+
+
