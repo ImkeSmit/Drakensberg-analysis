e6e28810cddb26fd2a18f7faec812d15b60baba2
ImkeSmit <imke610@gmail.com>
2026-02-12 11:06:11 +0200

    Create loop to plot timeseries of each logger

--- Scripts/data cleaning/Tomst plots.R
+++ Scripts/data cleaning/Tomst plots.R
@@ -6,25 +6,39 @@ library(ggpubr)
 #import data
 GG <- read.table("All_data/clean_data/Tomst_data/GoldenGate_tomst_data.txt", sep = "\t")
 
-TMS<- unique(GG$TMS)
-one_logger <- GG |> 
-  filter(TMS == TMS[1])
-
-soil_temp<-   ggplot(one_logger, aes(x = as.Date(datetime), y = soil_temp)) +
-  geom_line(col = "black") +
-  theme_classic()
-
-surface_temp <- ggplot(one_logger, aes(x = as.Date(datetime), y = surface_temp)) +
-  geom_line(col = "darkgreen") +
-  theme_classic()
-
-air_temp <- ggplot(one_logger, aes(x = as.Date(datetime), y = air_temp)) +
-  geom_line( col = "blue") +
-  theme_classic()
-
-soil_moist <- ggplot(one_logger, aes(x = as.Date(datetime), y = raw_moisture)) +
-  geom_line( col = "red") +
-  theme_classic()
+TMS_codes<- unique(GG$TMS)
 
 
+pdf("Figures//goldengate_loggers.pdf")
+for(t in 1:length(TMS_codes)) {
+  
+  one_logger <- GG[which(GG$TMS == TMS_codes[t]), ]
+  
+  soil_temp<- ggplot(one_logger, aes(x = as.Date(datetime), y = soil_temp)) +
+    geom_line(col = "black") +
+    theme_classic()+
+    labs(title = as.character(TMS_codes[t]))
+  
+  surface_temp <- ggplot(one_logger, aes(x = as.Date(datetime), y = surface_temp)) +
+    geom_line(col = "darkgreen") +
+    theme_classic()+
+    labs(title = as.character(TMS_codes[t]))
+  
+  air_temp <- ggplot(one_logger, aes(x = as.Date(datetime), y = air_temp)) +
+    geom_line( col = "blue") +
+    theme_classic()+
+    labs(title = as.character(TMS_codes[t]))
+  
+  soil_moist <- ggplot(one_logger, aes(x = as.Date(datetime), y = raw_moisture)) +
+    geom_line( col = "red") +
+    theme_classic()+
+    labs(title = as.character(TMS_codes[t]))
+  
+  print(soil_temp/surface_temp)
+  print(air_temp/soil_moist)
+  
+  #gridExtra::grid.arrange(soil_temp, surface_temp, air_temp, soil_moist, ncol = 1, nrow = 2)
+  
+}
 
+dev.off()
