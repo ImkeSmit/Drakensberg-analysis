9798f8806019216aaa0720bfd1ba2f72922300f7
ImkeSmit <imke610@gmail.com>
2026-03-16 12:16:46 +0200

    correct spelling

--- Scripts/data cleaning/Download_data_OSF.R
+++ Scripts/data cleaning/Download_data_OSF.R
@@ -7,6 +7,6 @@ library(dataDownloader)
 get_file(node = "374bk", #node of the Micro-climb OSF project
          file = "Differential_GPS_AUgust_2023.xlsx", #name of file you want to download
          path = "All_data/raw_data", #where you want to save this dataset
-         remote_path = "Clean_data/Geospatial_data") #Path on OSf to where this file is stored
+         remote_path = "Clean_data/Geospatial_data") #Path on OSF to where this file is stored
 
 #Now you can import the file as normal
