3d13dd44e0a6d40b710fb3c52037d8885f9b22da
ImkeSmit <imke610@gmail.com>
2026-03-17 09:51:05 +0200

    change spelling mistake

--- Scripts/data cleaning/Download_data_OSF.R
+++ Scripts/data cleaning/Download_data_OSF.R
@@ -10,7 +10,7 @@ library(osfr)
 #here is a vignette on how to do that: https://cran.r-project.org/web/packages/osfr/vignettes/auth.html
 
 get_file(node = "374bk", #node of the Micro-climb OSF project
-         file = "Differential_GPS_AUgust_2023.xlsx", #name of file you want to download
+         file = "Differential_GPS_August_2023.xlsx", #name of file you want to download
          path = "All_data/raw_data", #where you want to save this dataset
          remote_path = "Clean_data/Geospatial_data") #Path on OSF to where this file is stored
 
