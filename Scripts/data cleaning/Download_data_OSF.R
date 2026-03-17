41013582983ea4ccab837de936e7c9c1dc856362
ImkeSmit <imke610@gmail.com>
2026-03-17 09:53:55 +0200

    add instructions on how to set the PAt in your .Renviron file

--- Scripts/data cleaning/Download_data_OSF.R
+++ Scripts/data cleaning/Download_data_OSF.R
@@ -8,6 +8,12 @@ library(osfr)
 
 #Generate a personal access token on OSF and authenticate your PAT
 #here is a vignette on how to do that: https://cran.r-project.org/web/packages/osfr/vignettes/auth.html
+#You can put your token in a .Renviron file to automatically autheticate when you open your Rproject:
+#install.packages("usethis)
+#library(usethis)
+#usethis::::edit_r_environ("project")
+#Then edit the .Renviron in notepad or R with OSF_PAT=yourpersonalaccestoken
+#Restart R for it to take effect
 
 get_file(node = "374bk", #node of the Micro-climb OSF project
          file = "Differential_GPS_August_2023.xlsx", #name of file you want to download
