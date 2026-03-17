73efbe46d3ccfd3537e660d4f5aeb438927fc8e3
ImkeSmit <imke610@gmail.com>
2026-03-17 09:32:22 +0200

    instructions on using the PAT

--- Scripts/data cleaning/Download_data_OSF.R
+++ Scripts/data cleaning/Download_data_OSF.R
@@ -3,6 +3,11 @@ install.packages("remotes")
 library(remotes)
 remotes::install_github("Between-the-Fjords/dataDownloader") #install the dataDownloader package from github
 library(dataDownloader)
+install.packages("osfr")
+library(osfr)
+
+#Generate a personal access token on OSF and authenticate your PAT
+#here is a vignette on how to do that: https://cran.r-project.org/web/packages/osfr/vignettes/auth.html
 
 get_file(node = "374bk", #node of the Micro-climb OSF project
          file = "Differential_GPS_AUgust_2023.xlsx", #name of file you want to download
