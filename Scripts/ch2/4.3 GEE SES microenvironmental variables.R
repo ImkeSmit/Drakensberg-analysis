31def47de2e4d27c4312d35ea1a1b12152adce1c
ImkeSmit <imke610@gmail.com>
2026-04-22 12:37:04 +0200

    Clean up the script

--- Scripts/ch2/4.3 GEE SES microenvironmental variables.R
+++ Scripts/ch2/4.3 GEE SES microenvironmental variables.R
@@ -8,7 +8,7 @@ library(osfr)
 library(tidyverse)
 library(tidylog)
 
-#imort SES data
+#import SES data
 cell_ses <- read.csv("All_data/comm_assembly_results/RQ_weighted_cells_C5_entire.csv", row.names = 1) |> 
   mutate(elevation = case_when(grepl("BK", cellref) == T ~ "3000", #add elevation variable
                                grepl("WH", cellref) == T ~ "2500",
@@ -34,8 +34,7 @@ comb <- env |>
   select(-c(site, grid, column,row)) |> 
   inner_join(cell_ses, by = "Cell_ID") |> 
 ##Now we need to change the coordinates to reflect the spatial structure of the whole dataset
-##Coordinates within a site differ by 80
-##Coordinates between sites differ by 1000
+#make the grids contiguous, differing by 20m along the y axis
   mutate(y_new = case_when(site == "GG" ~ row, 
                            site == "WH" ~ row+160, 
                            site == "BK" ~ row+140, .default = NA)) |> 
@@ -54,49 +53,33 @@ comb <- env |>
   ungroup() 
 
 
-
 ###model for SES of height
 #isolate trait
 Hdat <- comb |> filter(trait == "Height_cm") |> 
                  drop_na(SES, rock_cover, mean_soil_depth) |> #remove rows with na's
-                select(Cell_ID,grid, x_coord, y_coord, SES, rock_cover, mean_soil_depth) 
+                select(Cell_ID,grid, x_coord, y_coord, SES, rock_cover, mean_soil_depth, elevation) 
 
-###We have to create the missing coordinates (between the girds and sites)
-y_sequence <- unique(Hdat$y_coord)[order(unique(Hdat$y_coord))]
-complete_y <- c(y_sequence[1]:y_sequence[length(y_sequence)])
-missing_ycoord <- setdiff(complete_y, y_sequence)
-missing_ycoord <- rep(missing_ycoord, each = 8) #repeat each ycoord 8 times, because there are 8 possible x coords
-missing_xcoord <- rep(c(1:8), length(unique(missing_ycoord)))
-#Create table to bind to Hdat
-addcoords <- tibble(Cell_ID = NA, x_coord = missing_xcoord, y_coord = missing_ycoord, 
-                    SES = NA, rock_cover = NA, mean_soil_depth = NA)
-#Hdat <- bind_rows(Hdat, addcoords) |> #add missing coordinates
-#  arrange(y_coord) 
+##We may have to impute values for missing cells to be able to run the gee model
 
+#isolate two grids to model for now
 Hdat2 <- Hdat |> filter(!is.na(SES)) |> 
   filter(grid %in% c("BK1", "BK2")) |>  #lets first only look at these grids because they are all the same size
   arrange(grid, y_coord) |> 
   mutate(grid = as.factor(grid))
-  
-#Select 3 complete grids to model, and the spaces between them
-#startrow <- which(Hdat$grid == "BK1")[1]
-#endrow <- which(Hdat$grid == "BK2")[length(which(Hdat$grid == "BK2"))]
 
-#Hdat2 <- Hdat[c(startrow:endrow), ]  
-
-
+#Coordinates to compute spatial relationships from
 coords <- cbind(Hdat2$x_coord, Hdat2$y_coord)
 
 # Compute pairwise distances WITHIN each grid
 # First, get one representative grid to build R from
 # (assuming all grids have the same 8x20 layout)
-Hdat |> group_by(grid) |> 
-  summarise(length(unique(Cell_ID)))
-
 grid1_dat <- Hdat2 |> filter(grid == unique(grid)[1])
 grid_coords <- cbind(grid1_dat$x_coord, grid1_dat$y_coord)
-
 dist_within <- as.matrix(dist(grid_coords))
+
+
+
+
 
 cutoff <- 5  # your autocorrelation range from variogram
 
@@ -105,8 +88,6 @@ R <- ifelse(dist_within <= cutoff, 1, 0)
 diag(R) <- 1  # gee::gee expects 1s on diagonal
 #This identifies which cells belong to the cluster to compute spatial weights within
 
-testlm <- lm(SES ~ rock_cover + mean_soil_depth,
-   family = "gaussian", data = Hdat2)
 
 gee2 <- gee::gee(SES ~ rock_cover + mean_soil_depth,
             family = gaussian, data = Hdat2,
@@ -118,13 +99,4 @@ gee2 <- gee::gee(SES ~ rock_cover + mean_soil_depth,
 summary(gee2)
 
 
-gee1 <- spind::GEE(SES ~ mean_soil_depth, 
-            family = gaussian, data = Hdat,
-            coord = coords, 
-            corstr = "exchangeable", #can be fixed, exchangeable or quadratic
-            #fixed means the same autocorrelation structure is applied to the whole dataset
-            cluster = 3,
-            scale.fix = FALSE)
-summary(gee1, printAutoCorPars = TRUE)
 
-plot(gee1)
