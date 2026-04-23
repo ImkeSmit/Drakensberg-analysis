e3002aeeea1c485952e33b10e39957cbfeef62fe
ImkeSmit <imke610@gmail.com>
2026-04-23 10:21:58 +0200

    fill in missing coordinate values

--- Scripts/ch2/4.3 GEE SES microenvironmental variables.R
+++ Scripts/ch2/4.3 GEE SES microenvironmental variables.R
@@ -22,17 +22,23 @@ cell_ses <- read.csv("All_data/comm_assembly_results/RQ_weighted_cells_C5_entire
          ncolumn = match(column, LETTERS[1:8])) |> 
           mutate(Cell_ID = paste0(site, "_G", str_sub(cellref, 3, 3), "_", column, row), 
                  elevation = as.numeric(elevation)) |> 
-          select(-cellref)
+          select(-c(cellref, site, grid, column, row)) |> 
+  filter(trait == "Height_cm")
 
 #import microenvironmental data
 env <- read.csv("All_data/clean_data/Environmental data/All_Sites_Environmental_Data.csv") |> 
-  mutate(mean_soil_depth = as.numeric(mean_soil_depth))
+  mutate(mean_soil_depth = as.numeric(mean_soil_depth), 
+         soil_moisture_adj_campaign2 = as.numeric(soil_moisture_adj_campaign2)) |> 
+  select(!c(soil_temperature_adj_campaign2, soil_moisture_adj_campaign1, soil_temperature_adj_campaign1,
+            aeolian_process, fluvial_process, slope_process,
+            geology1, geology2, geology3,
+            geology4, geology5, mesotopo, aspect, veg_max_height))
 
 
 ##Combine SES and environmental data
 comb <- env |> 
-  select(-c(site, grid, column,row)) |> 
-  inner_join(cell_ses, by = "Cell_ID") |> 
+  #select(-c(site, grid, column,row)) |> 
+  full_join(cell_ses, by = "Cell_ID") |> 
 ##Now we need to change the coordinates to reflect the spatial structure of the whole dataset
 #make the grids contiguous, differing by 20m along the y axis
   mutate(y_new = case_when(site == "GG" ~ row, 
@@ -52,7 +58,34 @@ comb <- env |>
          mean_soil_depth = as.numeric(mean_soil_depth)) |> 
   ungroup() 
 
-#
+#Check how many missing cells we have
+narows <- comb |> filter(if_any(everything(), is.na))
+
+comb |> filter(is.na(SES)) 
+
+comb |> group_by(trait, grid) |> 
+  summarise(n = n()) 
+
+###Fill in the x and y coordinates of cells that do not have SES
+comb2 <- comb |> 
+  mutate(ncolumn = match(column, LETTERS[1:8]),
+         elevation = case_when(grepl("BK", cellref) == T ~ "3000", #add elevation variable
+                               grepl("WH", cellref) == T ~ "2500",
+                               grepl("GG", cellref) == T ~ "2000",.default = NA),
+         y_new = case_when(site == "GG" ~ row, 
+                           site == "WH" ~ row+160, 
+                           site == "BK" ~ row+140, .default = NA)) |> 
+  group_by(site) |> 
+  mutate(y_coord = case_when(grepl("1", grid) ~ y_new, 
+                             grepl("2", grid) ~ y_new+20, 
+                             grepl("3", grid) ~ y_new+20*2,
+                             grepl("4", grid) ~ y_new+20*3, 
+                             grepl("5", grid) ~ y_new+20*4, 
+                             grepl("6", grid) ~ y_new+20*5, 
+                             grepl("7", grid) ~ y_new+20*6, 
+                             grepl("8", grid) ~ y_new+20*7, .default = NA)) |> 
+  mutate(x_coord = ncolumn) |> 
+  ungroup
 
 
 ###model for SES of height
