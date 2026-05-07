fc26964c67a7eaf34ef0c1b8b74d8a123961e5a4
ImkeSmit <imke610@gmail.com>
2026-05-07 11:37:15 +0200

    import data within function

--- Scripts/Function_cell_trait_coverage.R
+++ Scripts/Function_cell_trait_coverage.R
@@ -2,26 +2,17 @@
 #====Cell_trait_coverage==#
 #=========================#
 ####Function to identify cells that have trait measurements for less than 80% of the cover###
-abun_matrix <-read.csv("All_data/comm_assembly_results/abun_matrix.csv", row.names = 1) |> 
-  rownames_to_column(var = "Cell_ID") |> 
-  pivot_longer(cols = !Cell_ID, names_to = "taxon", values_to = "cover") |> 
-  filter(cover>0) |> 
-  mutate(
-    site = case_when(grepl("BK", Cell_ID) == T ~ "BK", #add elevation variable
-                     grepl("WH", Cell_ID) == T ~ "WH",
-                     grepl("GG", Cell_ID) == T ~ "GG",.default = NA),
-    grid = str_sub(Cell_ID, 1, 3), 
-    column = str_sub(Cell_ID, 4,4), 
-    row = as.numeric(str_sub(Cell_ID, 5,6)),
-    ncolumn = match(column, LETTERS[1:8])) |> 
-  mutate(Cell_ID = paste0(site, "_G", str_sub(Cell_ID, 3, 3), "_", column, row)) |> 
-  select(-c(site, grid, column, row, ncolumn))
-
-mean_traits <- read.csv("All_data/comm_assembly_results/mean_traits.csv", row.names = 1) |> 
-  rownames_to_column(var = "taxon")
 
 cell_trait_coverage <- function() {
-  abundances <- abun_matrix |> 
+  #import occurrence data without any sites or sp removed
+  drak <-read.csv("All_data/clean_data/micro_climb_occurrence.csv", row.names = 1) 
+  
+  #import mean traits
+  mean_traits <- read.csv("All_data/comm_assembly_results/mean_traits.csv", row.names = 1) |> 
+    rownames_to_column(var = "taxon")
+  
+  
+  abundances <- drak |> 
     filter(cover>0) #table with covers
   mean_traits <- mean_traits #table with mean traits for species
   
@@ -34,11 +25,19 @@ cell_trait_coverage <- function() {
       left_join(mean_traits, by = "taxon")
     
     total_cov <- sum(merge$cover)
+    
+    trait_cov <- merge |> 
+      filter(if_all(Height_cm:Thickness_mm, ~ !is.na(.)))
+    trait_cov<- sum(trait_cov$cover)
+    
+    trait_coverage <- trait_cov/total_cov
   }
   
-  
-  
-  
-  
-  
+  if(c == Cell_IDlist[1]) {
+    result <- cbind(c, trait_coverage)
+  }else {
+    temp<- cbind(c, trait_coverage)
+    result<- rbind(result, temp)
+  }
+  return(result)
 }
