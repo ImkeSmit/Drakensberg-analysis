52dd1667bca5e03be483be186eea3104d5912293
ImkeSmit <imke610@gmail.com>
2026-05-08 08:56:18 +0200

    remove unnecessary data handling now that we run it with the new composition data

--- Scripts/ch2/2.1.2 Investigate tails.R
+++ Scripts/ch2/2.1.2 Investigate tails.R
@@ -1,19 +1,7 @@
 ####Investigate long tails of SES###
 
 cell_ses <- read.csv("All_data/comm_assembly_results/RQ_weighted_cells_C5_entire.csv", row.names = 1) |> 
-  mutate(elevation = case_when(grepl("BK", cellref) == T ~ "3000", #add elevation variable
-                               grepl("WH", cellref) == T ~ "2500",
-                               grepl("GG", cellref) == T ~ "2000",.default = NA), 
-         site = case_when(grepl("BK", cellref) == T ~ "BK", #add elevation variable
-                          grepl("WH", cellref) == T ~ "WH",
-                          grepl("GG", cellref) == T ~ "GG",.default = NA),
-         grid = str_sub(cellref, 1, 3), 
-         column = str_sub(cellref, 4,4), 
-         row = as.numeric(str_sub(cellref, 5,6)), 
-         ncolumn = match(column, LETTERS[1:8])) |> 
-  mutate(Cell_ID = paste0(site, "_G", str_sub(cellref, 3, 3), "_", column, row), 
-         elevation = as.numeric(elevation)) |> 
-  select(-c(cellref, site, grid, column, row)) 
+  rename(Cell_ID = cellref)
 
 ggplot(cell_ses, aes(x = SES)) +
   geom_histogram()+
@@ -28,20 +16,10 @@ height_tails <- cell_ses |>
 abun_matrix <-read.csv("All_data/comm_assembly_results/abun_matrix.csv", row.names = 1) |> 
   rownames_to_column(var = "Cell_ID") |> 
   pivot_longer(cols = !Cell_ID, names_to = "taxon", values_to = "cover") |> 
-  filter(cover>0) |> 
-  mutate(
-  site = case_when(grepl("BK", Cell_ID) == T ~ "BK", #add elevation variable
-                   grepl("WH", Cell_ID) == T ~ "WH",
-                   grepl("GG", Cell_ID) == T ~ "GG",.default = NA),
-  grid = str_sub(Cell_ID, 1, 3), 
-  column = str_sub(Cell_ID, 4,4), 
-  row = as.numeric(str_sub(Cell_ID, 5,6)),
-  ncolumn = match(column, LETTERS[1:8])) |> 
-  mutate(Cell_ID = paste0(site, "_G", str_sub(Cell_ID, 3, 3), "_", column, row)) |> 
-  select(-c(site, grid, column, row)) 
+  filter(cover>0) 
 
 composition_height_tails <- height_tails |> 
-  inner_join(abun_matrix)
+  inner_join(abun_matrix, by = "Cell_ID")
 
 spnumber_height_tails <- composition_height_tails |> 
   group_by(Cell_ID) |> 
