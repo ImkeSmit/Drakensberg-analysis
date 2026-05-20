08b701dc0c06a846e7f2ba7553714cba5b003c02
ImkeSmit <imke610@gmail.com>
2026-05-20 14:25:44 +0200

    create script to run TMB on uncorrelated cells

---
+++ Scripts/ch2/4.3 glmmTMB subsampled SES microenvironmental variables.R
@@ -0,0 +1,86 @@
+###Modelling with glmmTMB on subsampled data####
+###Data is subsampled to eliminate spatial autocorrelation####
+#import SES data
+cell_ses <- read.csv("All_data/comm_assembly_results/RQ_weighted_cells_C5_entire.csv", row.names = 1) |> 
+  rename(Cell_ID = cellref)
+
+#import microenvironmental data
+env <- read.csv("All_data/clean_data/Environmental data/All_Sites_Environmental_Data.csv") |> 
+  mutate(mean_soil_depth = as.numeric(mean_soil_depth), 
+         soil_moisture_adj_campaign2 = as.numeric(soil_moisture_adj_campaign2)) |> 
+  dplyr::select(!c(soil_temperature_adj_campaign2, soil_moisture_adj_campaign1, soil_temperature_adj_campaign1,
+                   aeolian_process, fluvial_process, slope_process,
+                   geology1, geology2, geology3,
+                   geology4, geology5, mesotopo, aspect, veg_max_height))
+
+
+##Combine SES and environmental data
+comb <- env |> 
+  #join, one row in env matches many rows in cell_ses due to it containing ses of different traits
+  full_join(cell_ses, by = "Cell_ID", relationship = "one-to-many") |>
+  ##Now we need to change the coordinates to reflect the spatial structure of the whole dataset
+  #make the grids contiguous, differing by 20m along the y axis
+  mutate(elevation = as.factor(case_when(site == "GG" ~ 2000, 
+                                         site == "WH" ~ 2500, 
+                                         site == "BK" ~ 3000, .default = NA))) |> 
+  mutate(y_new = case_when(site == "GG" ~ row, 
+                           site == "WH" ~ row+160+1000, 
+                           site == "BK" ~ row+160+140+2000, .default = NA)) |> 
+  group_by(site) |> 
+  mutate(y_coord = case_when(grepl("1", grid) ~ y_new, 
+                             grepl("2", grid) ~ y_new+20+100, 
+                             grepl("3", grid) ~ y_new+20*2+200,
+                             grepl("4", grid) ~ y_new+20*3+300, 
+                             grepl("5", grid) ~ y_new+20*4+400, 
+                             grepl("6", grid) ~ y_new+20*5+500, 
+                             grepl("7", grid) ~ y_new+20*6+600, 
+                             grepl("8", grid) ~ y_new+20*7+700, .default = NA)) |> 
+  mutate(ncolumn = match(column, LETTERS[1:8]), 
+         x_coord = ncolumn, 
+         rock_cover = as.numeric(rock_cover), 
+         mean_soil_depth = as.numeric(mean_soil_depth)) |> 
+  ungroup() 
+
+
+###Fill in the x and y coordinates of cells that do not have SES
+comb2 <- comb |> 
+  mutate(ncolumn = match(column, LETTERS[1:8]),
+         elevation = as.factor(case_when(grepl("BK", Cell_ID) == T ~ "3000", #add elevation variable
+                                         grepl("WH", Cell_ID) == T ~ "2500",
+                                         grepl("GG", Cell_ID) == T ~ "2000", .default = NA)),
+         y_new = case_when(site == "GG" ~ row, 
+                           site == "WH" ~ row+160+1000, 
+                           site == "BK" ~ row+160 +140+2000, .default = NA)) |> 
+  group_by(site) |> 
+  mutate(y_coord = case_when(grepl("1", grid) ~ y_new, 
+                             grepl("2", grid) ~ y_new+20+100, 
+                             grepl("3", grid) ~ y_new+20*2+200,
+                             grepl("4", grid) ~ y_new+20*3+300, 
+                             grepl("5", grid) ~ y_new+20*4+400, 
+                             grepl("6", grid) ~ y_new+20*5+500, 
+                             grepl("7", grid) ~ y_new+20*6+600, 
+                             grepl("8", grid) ~ y_new+20*7+700, .default = NA)) |> 
+  mutate(x_coord = ncolumn, 
+         zrock_cover = c(scale(rock_cover)), #standardise variables
+         znorthness = c(scale(northness)), 
+         zsoil_moist = c(scale(soil_moisture_adj_campaign2)), 
+         zsoil_depth = c(scale(mean_soil_depth)), 
+         zslope_height = c(scale(slope_height))) |> 
+  ungroup()
+
+
+
+####SES HEIGHT####
+#isolate SES of height
+Hdat <- comb2 |> 
+  filter(trait %in% c("Height_cm", NA) |>  #also select cells which have no SES measurement. This is necessary to make the grid complete
+  arrange(y_coord, x_coord) |> 
+  mutate(trait = "Height_cm",  #give all records a trait
+         grid = as.factor(paste0(site, grid)), 
+         pos = numFactor(x_coord, y_coord))
+  
+  
+###Sample uncorrelated cells  
+#Run Function_select_independent_cells.R
+Hdat_subs<- select_independent_cells(Hdat, grid_var = "grid", x = "x_coord", y = "y_coord", value_col = "SES",
+                                max_search_radius = 3, lag_threshold = 4)
