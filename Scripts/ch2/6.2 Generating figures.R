543da10845bf7389dc4509fd2930335ce85d5047
ImkeSmit <imke610@gmail.com>
2026-05-25 12:59:44 +0200

    add hypothesis figures

--- Scripts/ch2/6.2 Generating figures.R
+++ Scripts/ch2/6.2 Generating figures.R
@@ -1,6 +1,105 @@
 ####Script to generate figures for posters and publications####
 library(ggplot)
 library(ggridges)
+library(tidyverse)
+
+####Hypothesis figure SES~elevation####
+#generate fake data
+SESlow <- dnorm(200, mean = 1, sd = 0.2)
+SESmid <- dnorm(200, mean = 0, sd = 0.2)
+SEShigh <- dnorm(200, mean = -1, sd = 0.2)
+SES_all <- c(SESlow, SESmid, SEShigh)
+
+hp_dat <- tibble(elevation = c(rep("2000", 200), rep("2500", 200), rep("3000", 200)), 
+                 SES = SES_all)
+
+hypothesis_ses_ridges <- hp_dat |>
+  ggplot(aes(x = SES, y = elevation)) +
+  geom_density_ridges(alpha = 0.5) +
+  labs(x = "SES", y = "Elevation (m a.s.l.)") +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  theme_bw() +
+  theme(legend.position = "none", axis.title = element_text(size = 20), 
+        axis.text = element_text(size = 18), 
+        panel.grid = element_blank()) 
+ggsave(hypothesis_ses_ridges, filename = "hypothesis_SES_elevation_poster.png", path = "Figures", 
+       width = 1100, height = 1100, units = "px")
+
+
+
+####Hypothesis grid maps####
+##Function to retrieve data of one grid
+one_grid_raster <- function(data, variable, g) {
+  
+  library(sp)
+  library(raster)
+  
+  # filter data for this grid
+  g_dat <- data %>% filter(grid == g)
+  
+  
+  #remove extra columns
+  g_dat <- g_dat[, which(colnames(g_dat) %in% c("row", "ncolumn", variable))]
+  
+  # Create spatial grid object
+  x_range <- 1:20
+  y_range <- 1:8
+  
+  grid_obj <- expand.grid(x = x_range, y = y_range)
+  coordinates(grid_obj) <- ~x + y
+  gridded(grid_obj) <- TRUE
+  r <- raster(grid_obj) 
+  
+  #Check if there are missing cells in data
+  possible_cells <- paste(grid_obj$x, grid_obj$y, sep = "_")
+  observed_cells <- paste(g_dat$row, g_dat$ncolumn, sep = "_")
+  missing <- which(is.na(match(possible_cells, observed_cells)))
+  
+  if(length(missing) > 0) { #create a filler
+    
+    filler <- data.frame(row = grid_obj$x[missing], ncolumn = grid_obj$y[missing])
+    filler[[variable]] <- NA #the variable is set to NA
+    
+    g_dat <- rbind(g_dat, filler) }
+  
+  # need values sorted to match the raster cell order:
+  g_dat <- g_dat[order(g_dat$ncolumn, g_dat$row), ]
+  g_dat <- as.data.frame(g_dat)
+  
+  return(g_dat)
+  #end function
+}
+
+##import environmental data
+env <- read.csv("All_data/clean_data/Environmental data/All_Sites_Environmental_Data.csv") |> 
+  mutate(elevation = case_when(grepl("BK", Cell_ID) == T ~ "3000", #add elevation variable
+                               grepl("WH", Cell_ID) == T ~ "2500",
+                               grepl("GG", Cell_ID) == T ~ "2000",.default = NA)) |> 
+  dplyr::select(-c("site", "grid")) |> 
+  separate_wider_delim(Cell_ID, delim = "_", names = c("site", "grid", "cell"), cols_remove = F) |> 
+  mutate(grid = paste0(site, grid), 
+         column = substr(cell, 1,1), 
+         row = substr(cell, 2,3),
+         ncolumn = match(column, LETTERS[1:8])) |> 
+  mutate(mean_soil_depth = as.numeric(mean_soil_depth), 
+         soil_moisture_adj_campaign2 = as.numeric(soil_moisture_adj_campaign2)) |> 
+  dplyr::select(!c(soil_temperature_adj_campaign2, soil_moisture_adj_campaign1, soil_temperature_adj_campaign1,
+                   aeolian_process, fluvial_process, slope_process,
+                   geology1, geology2, geology3,
+                   geology4, geology5, mesotopo, aspect, veg_max_height))
+
+
+WHG7_rock_r <- one_grid_raster(data = env, variable = "rock_cover", g = "WHG7")
+
+grid_rock_WHG7 <- ggplot(WHG7_rock_r, aes(row, ncolumn, fill = rock_cover))+
+  geom_raster()+
+  scale_fill_gradient(low = "white", high = "darkorange2", na.value = "pink")+
+  theme_void()+
+  labs(x = " ", y = " ", fill = "Rock cover (%)")  +
+  theme(legend.text = element_text(size = 16), legend.title = element_text(size = 20))
+ggsave("Figures//rock_cover_WHG7.png", grid_rock_WHG7, device = png, width = 2000, units = "px")
+
+
 
 #####SES ~ elevation ridges####
 #import SES data
