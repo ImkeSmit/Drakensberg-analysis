a61104b341a2649be695d438b00dda904d52b0b7
ImkeSmit <imke610@gmail.com>
2026-06-15 14:59:11 +0200

    some more data wrangling before the PCA

--- Scripts/ch2/3.5 PCA environmental variables.R
+++ Scripts/ch2/3.5 PCA environmental variables.R
@@ -9,7 +9,11 @@ env <- read.csv("All_data/clean_data/Environmental data/All_Sites_Environmental_
          soil_depth_CV = as.numeric(soil_depth_CV)) |> 
   #variables we are interested in
   select(c(Cell_ID:row, rock_cover, northness, soil_moisture_adj_campaign2, 
-           soil_depth_CV, mean_soil_depth, slope_height)) 
+           soil_depth_CV, mean_soil_depth, slope_height)) |> 
+  #add elevation variables
+  mutate(elevation = case_when(site == "GG" ~ 2000, 
+                               site == "WH" ~ 2500, 
+                               site == "BK" ~ 3000, .default = NA))
 
 ###Microclimate indices for the summer
 ind <- read.csv("C:\\Users\\imke6\\Documents\\PhD 2025\\Ch2 niche modelling\\All_data\\Clean_data\\microclimate\\Pekka_cleaned_data\\TOMST data\\transfer_402058_files_56a24364\\microclimate_indices.csv") |> 
@@ -22,24 +26,40 @@ ind <- read.csv("C:\\Users\\imke6\\Documents\\PhD 2025\\Ch2 niche modelling\\All
   select(!c(site, impprop)) |> 
   rename(site = "area") |> 
   #pick variables we are interested in
-  filter(stat %in% c("avg", "fdh", "gdh5"), sensor %in% c("T1", "moist")) |>  #T1 is the soil temperature, this is the most relaible temperature as it isn't excesively heated by solar radiation
-  mutate(variable = paste(sensor, stat, sep = "_")) |> 
-  select(!c(sensor, stat)) |> 
-  pivot_wider(names_from = variable, values_from = value)
+  filter(stat %in% c("avg", "fdh", "gdh5"), sensor %in% c("T1", "moist"), period %in% c("annual", "summer")) |>  #T1 is the soil temperature, this is the most relaible temperature as it isn't excesively heated by solar radiation
+  mutate(variable = paste(sensor, stat, period, sep = "_")) |> 
+  select(!c(sensor, stat, period)) |> 
+  pivot_wider(names_from = variable, values_from = value) |> 
+  arrange(site, grid)
 
-#look at ranges
-hist(ind$value_T1_gdh5) 
-hist(ind$value_T1_fdh)
-hist(ind$value_T1_avg)
-hist(ind$value_moist_avg)
+ind %>% 
+  group_by(Cell_ID) %>% 
+  filter(n() > 1) %>% 
+  ungroup() #no duplicates here
 
 
 ####Remote sensing derived variables
 rms <- read.csv("All_data/clean_data/Environmental data/Zonal_stats_all.csv") |> 
   rename(Cell_ID = CELL_ID)
 
+dups <- rms %>% 
+  group_by(Cell_ID) %>% 
+  filter(n() > 1) %>% 
+  ungroup() #There are some duplicates. For now we will just remove them
+
+rms <- rms |> 
+  filter(!(Cell_ID %in% c(dups$Cell_ID)))
+
 
 ###Now we merge them all together
 all_env <- env |> 
   full_join(ind, by = "Cell_ID") |> 
-  full_join(rms, by = "Cell_ID")
+  full_join(rms, by = "Cell_ID")  |> 
+  select(c(Cell_ID, rock_cover:slope_height, T1_avg_annual:moist_avg_summer, STD)) |> 
+  column_to_rownames(var = "Cell_ID") |> 
+  mutate(across(c(rock_cover: STD), ~ as.numeric(scale(.x))))
+  
+  
+
+
+
