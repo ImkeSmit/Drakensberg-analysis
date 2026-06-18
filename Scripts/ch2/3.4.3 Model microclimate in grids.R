42dd2160f5d4b446473d6d87336ab604938fe954
ImkeSmit <imke610@gmail.com>
2026-06-18 15:02:11 +0200

    choose second bets model when th enull model is selected

--- Scripts/ch2/3.4.3 Model microclimate in grids.R
+++ Scripts/ch2/3.4.3 Model microclimate in grids.R
@@ -21,14 +21,14 @@ env <- read.csv("All_data/clean_data/Environmental data/All_Sites_Environmental_
          mesotopo = as.factor(mesotopo)) |> 
   #variables we are interested in
   dplyr::select(c(Cell_ID:row, vascular_cover, rock_cover, soil_cover, northness, eastness, 
-                  mesotopo, veg_max_height, mean_soil_depth, slope_height)) 
+                  mesotopo, veg_max_height, mean_soil_depth, slope_height, MAX)) 
 
 
 ###Correlation of env variables
 cordf <- env |> 
   dplyr::select(!mesotopo) |> 
   drop_na()
-cormat <- cor(cordf[, c(6:13)])
+cormat <- cor(cordf[, c(6:14)])
 corrplot(cormat, method = "number", type = "lower", 
          tl.cex = 0.8,    # shrink variable name text
          tl.srt = 45)
@@ -61,16 +61,41 @@ for(g in 1:length(gridlist)) {
   
   #full models
   grid_fullmod_T1 <- lm(mean_T1_growing_season ~ rock_cover+ soil_cover+ northness+ eastness+ 
-                      veg_max_height+ mean_soil_depth+ slope_height, data = one_grid)
+                      veg_max_height+ mean_soil_depth+ slope_height +MAX, data = one_grid)
   #does not contain mesotopo because the model tries to predict to novel mesotopo codes
   
   grid_fullmod_moist <- lm(mean_moist_growing_season ~ rock_cover+ soil_cover+ northness+ eastness+ 
-                         veg_max_height+ mean_soil_depth+ slope_height, data = one_grid)
+                         veg_max_height+ mean_soil_depth+ slope_height + MAX, data = one_grid)
   
   #backward selection
   T1_bestmod <- stepAIC(grid_fullmod_T1, direction = "backward")
   
-  moist_bestmod <- stepAIC(grid_fullmod_moist, direction = "backward")
+  # Store all models visited during stepping, because moistmod sometimes selcts the null model
+  visited_models <- list()
+  
+  moist_bestmod <- stepAIC(
+    grid_fullmod_moist,
+    direction = "backward",
+    trace = TRUE,
+    keep = function(model, aic) {
+      visited_models[[length(visited_models) + 1]] <<- model  # <<- assigns to parent env
+      list(formula = formula(model), aic = aic)
+    }
+  )
+  
+  # Compare formula as string to check if best model is the null model
+  if (deparse(formula(moist_bestmod)) == deparse(as.formula(mean_moist_growing_season ~ 1))) {
+    
+    # Extract AICs from visited models
+    all_aics     <- sapply(visited_models, AIC)
+    
+    # Sort and get second lowest
+    sorted_idx       <- order(all_aics)
+    second_best_form <- formula(visited_models[[sorted_idx[2]]])
+    
+    # Refit
+    moist_bestmod <- update(moist_bestmod, formula = second_best_form)
+  }
   
   
   #predictions
@@ -90,8 +115,10 @@ for(g in 1:length(gridlist)) {
   pred_dat_save <- pred_dat |> 
     mutate(predicted_mean_T1_growing_season = predicted_mean_T1_growing_season, 
            predicted_mean_moist_growing_season = predicted_mean_moist_growing_season,
-           T1_bestmod_formula = paste0(as.character(formula(T1_bestmod))), 
-           moist_bestmod_formula = as.character(formula (moist_bestmod))) 
+           T1_bestmod_formula = as.character(summary(T1_bestmod)$call)[2], 
+           moist_bestmod_formula = as.character(summary(moist_bestmod)$call)[2], 
+           T1_bestmod_rsq = summary(T1_bestmod)$adj.r.squared, 
+           moist_bestmod_rsq = summary(moist_bestmod)$adj.r.squared) 
   
   if(g == 1) {
     result <- pred_dat_save
@@ -99,7 +126,6 @@ for(g in 1:length(gridlist)) {
     temp_result <- pred_dat_save
     result <- bind_rows(result, temp_result)
   }
-    
 }
 
 
