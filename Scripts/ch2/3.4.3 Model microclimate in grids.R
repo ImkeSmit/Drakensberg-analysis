4d5aabd0260b21cea8e98af2b3f04d2d9a848cdb
ImkeSmit <imke610@gmail.com>
2026-06-18 09:54:10 +0200

    import the environmental data

--- Scripts/ch2/3.4.3 Model microclimate in grids.R
+++ Scripts/ch2/3.4.3 Model microclimate in grids.R
@@ -9,3 +9,18 @@ ind <- read.csv("all_data/clean_data/Environmental data/Imke_microclimate_indice
          column = str_sub(Cell_ID, 7,7), 
          row = as.numeric(str_sub(Cell_ID, 8,9)), 
          ncolumn = match(column, LETTERS[1:8]))
+
+
+###Import environmental data
+env <- read.csv("All_data/clean_data/Environmental data/All_Sites_Environmental_Data.csv") |> 
+  mutate(mean_soil_depth = as.numeric(mean_soil_depth), 
+         soil_moisture_adj_campaign2 = as.numeric(soil_moisture_adj_campaign2), 
+         soil_depth_CV = as.numeric(soil_depth_CV), 
+         mesotopo = as.factor(mesotopo)) |> 
+  #variables we are interested in
+  dplyr::select(c(Cell_ID:row, vascular_cover, rock_cover, soil_cover, northness, eastness, 
+                  mesotopo, veg_max_height, mean_soil_depth, slope_height)) |> 
+  mutate(zrock_cover = c(scale(rock_cover)), #standardise variables
+         znorthness = c(scale(northness)), 
+         zsoil_depth = c(scale(mean_soil_depth)), 
+         zslope_height = c(scale(slope_height)))
