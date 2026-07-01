6271e7289e5558354c2f4f2c9ebd3623127e6843
ImkeSmit <imke610@gmail.com>
2026-07-01 10:01:56 +0200

    We use the mininmum temps to define growing season, so delete the code that uses mean temps

--- Scripts/ch2/3.4.2 Create microclimate indices.R
+++ Scripts/ch2/3.4.2 Create microclimate indices.R
@@ -47,58 +47,29 @@ ggplot(min_daily_temp, aes( x = date, y = min_daily_T1)) +
   facet_wrap(~site, nrow = 3, ncol = 1)
 
 ####===DEFINE GROWING SEASON===####
-###USING MEAN TEMP####
-###Period where mean annual temp is consistently above 5 degrees
-#first date after June 21 where all logers at a site had a mean  temp above 5
-start_grw <- mean_daily_temp |>
-  filter(date > ymd("2023-06-21")) |>           # only dates after 21 Jun 2023
-  group_by(site, date) %>%
-  summarise(all_above_5 = all(mean_daily_T1 > 5),    # TRUE only if every Cell_ID > 5
-            .groups = "drop") |> 
-  filter(all_above_5) |> #only keep dates where all loggers had temps above 5
-  group_by(site) |> 
-  slice_min(date, n = 1) |>   #earliest date per site
-  rename(start_grw_date = date) |> 
-  select(!all_above_5)
-
-##latest date before 21 June where all loggers at a site had a mean temp above 5
-end_grw <- mean_daily_temp |>
-  filter(date < ymd("2023-06-21")) |>           # only dates before 21 Jun 2023
-  group_by(site, date) %>%
-  summarise(all_above_5 = all(mean_daily_T1 > 5),    # TRUE only if every Cell_ID > 5
-            .groups = "drop") |> 
-  filter(all_above_5) |> #only keep dates where all loggers had temps above 5
-  group_by(site) |> 
-  slice_max(date, n = 1) |>   #latest date per site
-  rename(end_grw_date = date) |> 
-  select(!all_above_5)
-
-growing_season_mean_temps <- start_grw |> 
-  left_join(end_grw, by = "site") #do not consider the years, only the days!
-
 
 
 ###USING MIN TEMP####
 ###Period where mean annual temp is consistently above 5 degrees
-#first date after June 21 where all logers at a site had a mean  temp above 5
+#first date after June 21 where all logers at a site had a min  temp above 5
 start_grw <- min_daily_temp |>
   filter(date > ymd("2023-06-21")) |>           # only dates after 21 Jun 2023
   group_by(site, date) %>%
   summarise(all_above_5 = all(min_daily_T1 > 5),    # TRUE only if every Cell_ID > 5
             .groups = "drop") |> 
-  filter(all_above_5) |> #only keep dates where all loggers had temps above 5
+  filter(all_above_5) |> #only keep dates where all loggers had min temps above 5
   group_by(site) |> 
   slice_min(date, n = 1) |>   #earliest date per site
   rename(start_grw_date = date) |> 
   dplyr::select(!all_above_5)
 
-##latest date before 21 June where all loggers at a site had a mean temp above 5
+##latest date before 21 June where all loggers at a site had a min temp above 5
 end_grw <- min_daily_temp |>
   filter(date < ymd("2023-06-21")) |>           # only dates before 21 Jun 2023
   group_by(site, date) %>%
   summarise(all_above_5 = all(min_daily_T1 > 5),    # TRUE only if every Cell_ID > 5
             .groups = "drop") |> 
-  filter(all_above_5) |> #only keep dates where all loggers had temps above 5
+  filter(all_above_5) |> #only keep dates where all loggers had min temps above 5
   group_by(site) |> 
   slice_max(date, n = 1) |>   #latest date per site
   rename(end_grw_date = date) |> 
