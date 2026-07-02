26ae8749cbd0976a090887a859a21e6c794ead3b
ImkeSmit <imke610@gmail.com>
2026-07-02 11:04:59 +0200

    paste improved data import code

--- Scripts/ch2/3.5 PCA environmental variables.R
+++ Scripts/ch2/3.5 PCA environmental variables.R
@@ -3,40 +3,37 @@ library(tidyverse)
 library(tidylog)
 library(ggbiplot)
 
-###Microenvironmental data
+#import microenvironmental data
 env <- read.csv("All_data/clean_data/Environmental data/All_Sites_Environmental_Data.csv") |> 
-  mutate(mean_soil_depth = as.numeric(mean_soil_depth), 
-         soil_moisture_adj_campaign2 = as.numeric(soil_moisture_adj_campaign2), 
-         soil_depth_CV = as.numeric(soil_depth_CV)) |> 
   #variables we are interested in
-  dplyr::select(c(Cell_ID:row, rock_cover, northness, soil_moisture_adj_campaign2, 
-           soil_depth_CV, mean_soil_depth, slope_height)) |> 
+  select(Cell_ID:row, rock_cover, northness, soil_moisture_adj_campaign2, 
+         soil_depth_CV, mean_soil_depth, slope_height) |> 
   #add elevation variables
   mutate(elevation = case_when(site == "GG" ~ 2000, 
                                site == "WH" ~ 2500, 
-                               site == "BK" ~ 3000, .default = NA))
+                               site == "BK" ~ 3000,
+                               .default = NA))
 
-###Microclimate indices for the summer
-ind <- read.csv("C:\\Users\\imke6\\Documents\\PhD 2025\\Ch2 niche modelling\\All_data\\Clean_data\\microclimate\\Pekka_cleaned_data\\TOMST data\\transfer_402058_files_56a24364\\microclimate_indices.csv") |> 
-  mutate(area = case_when(area == "BOK" ~ "BK", 
-                          area == "GOL" ~ "GG", 
-                          area == "WIT" ~ "WH"), 
-         grid = str_split_i(grid, "_", 2), 
-         cell = toupper(str_split_i(site, "_", 3)), 
-         Cell_ID = paste0(area, "_G", grid, "_", cell)) |> 
-  dplyr::select(!c(site, impprop)) |> 
-  rename(site = "area") |> 
-  #pick variables we are interested in
-  filter(stat %in% c("avg", "fdh", "gdh5"), sensor %in% c("T1", "moist"), period %in% c("annual", "summer")) |>  #T1 is the soil temperature, this is the most relaible temperature as it isn't excesively heated by solar radiation
-  mutate(variable = paste(sensor, stat, period, sep = "_")) |> 
-  dplyr::select(!c(sensor, stat, period)) |> 
-  pivot_wider(names_from = variable, values_from = value) |> 
-  arrange(site, grid)
+#import remote sensing derived variables
+rms <- read.csv("All_data/clean_data/Environmental data/Zonal_stats_all.csv") |> 
+  select(CELL_ID, STD) |> 
+  rename(Cell_ID = CELL_ID)
 
-ind %>% 
-  group_by(Cell_ID) %>% 
-  filter(n() > 1) %>% 
-  ungroup() #no duplicates here
+#import interpolated microclimate indices
+micro_idw <- read.csv("All_data/clean_data/Environmental data/Imke_microclimate_indices_idw_interpolated.csv", row.names = 1)
+
+
+##Combine SES and environmental data
+comb <- env |> 
+  #join, one row in env matches many rows in cell_ses due to it containing ses of different traits
+  full_join(cell_ses, by = "Cell_ID", relationship = "one-to-many") |>
+  #join to microclimate indices |> 
+  full_join(micro_idw, by = "Cell_ID") |> 
+  #join to remote sensing data |> 
+  full_join(rms, by = "Cell_ID") |> 
+  mutate(ncolumn = match(column, LETTERS[1:8])) |> 
+  rename(x_coord = ncolumn, 
+         y_coord = row)
 
 
 ####Remote sensing derived variables
