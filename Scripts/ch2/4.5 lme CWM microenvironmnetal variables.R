159c571ceb4fbbc5f0a1ff6b9f11b68e3437dd54
ImkeSmit <imke610@gmail.com>
2026-07-10 11:05:26 +0200

    try to write the loop to predict over every env variable

--- Scripts/ch2/4.5 lme CWM microenvironmnetal variables.R
+++ Scripts/ch2/4.5 lme CWM microenvironmnetal variables.R
@@ -1,4 +1,5 @@
 ###Models of CWm ~ microenvironmental variables with lme####
+###And predictions of CWm traits over environmental gradients####
 library(tidyverse)
 library(tidylog)
 library(nlme)
@@ -19,8 +20,7 @@ abun_matrix <-read.csv("All_data/comm_assembly_results/abun_matrix.csv", row.nam
 mean_traits <- read.csv("All_data/comm_assembly_results/mean_traits.csv", row.names = 1) |> 
   mutate(log_Height = log(Height_cm), 
          log_LA = log(Leaf_area_mm2), 
-         log_LDMC = log(LDMC)) |> #use the same log variables as we do in the models
-  select(!c(LDMC, Leaf_area_mm2, Height_cm, Thickness_mm))
+         log_LDMC = log(LDMC))
 
 
 #compute CWM of each trait for each cell
@@ -29,47 +29,75 @@ cwm <- functcomp(x = mean_traits, a = as.matrix(abun_matrix))
 cwm2 <- cwm |>
   rownames_to_column(var = "cellref") |> 
   rename(Cell_ID = cellref) |> 
-  mutate(elevation = case_when(grepl("BK", Cell_ID) == T ~ "3000", #add elevation variable
-                               grepl("WH", Cell_ID) == T ~ "2500",
-                               grepl("GG", Cell_ID) == T ~ "2000",.default = NA)) |> 
-  pivot_longer(cols = c("log_Height", "log_LDMC", "log_LA", "SLA"), names_to = "trait", values_to = "cwm_value")
+  pivot_longer(cols = !Cell_ID, names_to = "trait", values_to = "cwm_value")
 
-#import env data so that we can join x and y coordinates
-env <- read.csv("All_data/clean_data/Environmental data/All_Sites_Environmental_Data.csv") |> 
+#import microenvironmental data
+env2 <- read.csv("All_data/clean_data/Environmental data/All_Sites_Environmental_Data.csv") |> 
   #variables we are interested in
-  select(Cell_ID:row) 
+  select(Cell_ID:row, rock_cover, northness, soil_moisture_adj_campaign2, 
+         soil_depth_CV, mean_soil_depth, slope_height) |> 
+  #add elevation variables
+  mutate(elevation = case_when(site == "GG" ~ 2000, 
+                               site == "WH" ~ 2500, 
+                               site == "BK" ~ 3000,
+                               .default = NA))
 
-cwm_comb <- env |> 
+#import remote sensing derived variables
+rms <- read.csv("All_data/clean_data/Environmental data/Zonal_stats_all.csv") |> 
+  select(CELL_ID, STD) |> 
+  rename(Cell_ID = CELL_ID)
+
+#import interpolated microclimate indices
+micro_idw <- read.csv("All_data/clean_data/Environmental data/Imke_microclimate_indices_idw_interpolated.csv", row.names = 1)
+
+
+##Combine CWM and environmental data
+cwm_comb <- env2 |> 
+  #join to microclimate indices |> 
+  full_join(micro_idw, by = "Cell_ID") |> 
+  #join to remote sensing data |> 
+  full_join(rms, by = "Cell_ID") |> 
   #join, one row in env matches many rows in cell_ses due to it containing ses of different traits
-  inner_join(cwm2, by = "Cell_ID", relationship = "one-to-many") |>
+  full_join(cwm2, by = "Cell_ID", relationship = "one-to-many") |>
   mutate(ncolumn = match(column, LETTERS[1:8]), 
          grid = paste0(site, grid)) |> #each grid must have a unique id 
   rename(x_coord = ncolumn, 
          y_coord = row)
 
 
-####model CWM ~ elevation####
-traitlist <- c("log_Height", "log_LDMC", "log_LA", "SLA")
-#lists to store results in
-CWM_ele_cld<- vector(mode= "list", length = length(traitlist))
-names(CWM_ele_cld) = traitlist
+####model CWM ~ microenvironmental variables####
+traitlist <- c("Height_cm","Leaf_area_mm2", "log_LDMC","SLA") #the traits we chose to log here are different to the traits logged in the elevation model.
+sitelist <- c("GG", "WH", "BK")
+variables <- c("mean_T1_growing_season" , "mean_moist_growing_season" , "rock_cover", "mean_soil_depth" , "STD")
+
+
 
 for (t in 1:length(traitlist)) {
+  for(s in 1:length(sitelist)) {
   modeldat <-  cwm_comb |> 
-    filter(trait == traitlist[t]) |> 
+    filter(trait == traitlist[t], 
+           site == sitelist[s]) |> 
     mutate(elevation = as.factor(elevation), 
            grid = as.factor(grid)) |> 
     drop_na()
   
   
-  model<- lme(cwm_value ~ elevation ,
+  model<- lme(cwm_value ~ mean_T1_growing_season + mean_moist_growing_season + rock_cover+ mean_soil_depth + STD ,
               random = ~1|grid, 
               correlation = corSpher(form = ~ x_coord + y_coord|grid, nugget = TRUE), #spherical structure
               data = modeldat) #only gaussian family possible
   
-  em_model <- emmeans(model, specs = "elevation", type = "response")
-  comp_letters <-cld(em_model, Letters = letters, adjust = "Tukey", sort = FALSE)
+  for(v in 1:length(variables)) {
+    pred_var <- variables[v]
+    mean_var <- setdiff(variables, variables[v])
+    
+    pred_dat <- modeldat |> 
+      mutate(accross(mean_var), ~ mean(.x, na.rm = TRUE))
+    
+    
+    
+    
+  }
   
-  #Also save results in a list object so we can call it with quarto
-  CWM_ele_cld[[t]] <- comp_letters
-}
+  
+}}
