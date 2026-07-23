1dd53b8e98bd58b4650747e64f18104a08fee28a
ImkeSmit <imke610@gmail.com>
2026-07-23 10:30:18 +0200

    Change to unique variable names

--- Scripts/ch2/4.4 lme SES microenvironmental variables.R
+++ Scripts/ch2/4.4 lme SES microenvironmental variables.R
@@ -62,29 +62,27 @@ C5_cell_ses_poolentire <- read.csv("All_data/comm_assembly_results/SES_RQ_weight
 ##Combine SES and environmental data
 C5_ele_comb <- pos |> 
   #join, one row in env matches many rows in cell_ses due to it containing ses of different traits
-  inner_join(cell_ses, by = "Cell_ID", relationship = "one-to-many") 
+  inner_join(C5_cell_ses_poolentire, by = "Cell_ID", relationship = "one-to-many") 
   
 #These variables have the best diagnostics
 #LA and SLA still have TERRIBLE DIAGNOSTICS
 traitlist <- c("log_Height", "log_LDMC", "log_LA", "SLA")
 #lists to store results in
-SES_ele_summary <- vector(mode= "list", length = length(traitlist))
-names(SES_ele_summary) = traitlist
+C5_SES_ele_summary <- vector(mode= "list", length = length(traitlist))
+names(C5_SES_ele_summary) = traitlist
 
-SES_ele_Rsq<- vector(mode= "list", length = length(traitlist))
-names(SES_ele_Rsq) = traitlist
+C5_SES_ele_Rsq<- vector(mode= "list", length = length(traitlist))
+names(C5_SES_ele_Rsq) = traitlist
 
-SES_ele_anova<- vector(mode= "list", length = length(traitlist))
-names(SES_ele_anova) = traitlist
+C5_SES_ele_anova<- vector(mode= "list", length = length(traitlist))
+names(C5_SES_ele_anova) = traitlist
 
-SES_ele_cld<- vector(mode= "list", length = length(traitlist))
-names(SES_ele_cld) = traitlist
+C5_SES_ele_cld<- vector(mode= "list", length = length(traitlist))
+names(C5_SES_ele_cld) = traitlist
 
 for (t in 1:length(traitlist)) {
-  modeldat <-  comb |> 
+  modeldat <-  C5_ele_comb |> 
     filter(trait == traitlist[t]) |> 
-    mutate(elevation = as.factor(elevation), 
-           grid = as.factor(grid)) |> 
     drop_na()
   
   
@@ -154,12 +152,13 @@ for (t in 1:length(traitlist)) {
   sink()
   
   #Also save results in a list object so we can call it with quarto
-  SES_ele_summary[[t]] <- summary(model)
-  SES_ele_Rsq[[t]] <- r.squaredGLMM(model)
-  SES_ele_anova[[t]] <- anova(model)
-  SES_ele_cld[[t]] <- comp_letters
+  C5_SES_ele_summary[[t]] <- summary(model)
+  C5_SES_ele_Rsq[[t]] <- r.squaredGLMM(model)
+  C5_SES_ele_anova[[t]] <- anova(model)
+  C5_SES_ele_cld[[t]] <- comp_letters
   
 }
+rm(model, modeldat) #remove to make sure we don't carry the wrong object over
 
 ###Using the logged traits are generally better for model diagnostics than the raw traits
 ##BUT the leaf area diagnostics are still very bad
@@ -172,46 +171,25 @@ for (t in 1:length(traitlist)) {
 ###=============C5 NULL MODEL, pool = site====================####
 ###===========================================================###
 #import ses
-cell_ses_poolsite <- read.csv("All_data/comm_assembly_results/SES_RQ_weighted_cells_C5_poolsite.csv", row.names = 1) |> 
+C5_cell_ses_poolsite <- read.csv("All_data/comm_assembly_results/SES_RQ_weighted_cells_C5_poolsite.csv", row.names = 1) |> 
   rename(Cell_ID = cellref) 
-
-#import microenvironmental data
-env2 <- read.csv("All_data/clean_data/Environmental data/All_Sites_Environmental_Data.csv") |> 
-  #variables we are interested in
-  select(Cell_ID:row, rock_cover, northness, soil_moisture_adj_campaign2, 
-         soil_depth_CV, mean_soil_depth, slope_height) |> 
-  #add elevation variables
-  mutate(elevation = case_when(site == "GG" ~ 2000, 
-                               site == "WH" ~ 2500, 
-                               site == "BK" ~ 3000,
-                               .default = NA))
-
-#import remote sensing derived variables
-rms <- read.csv("All_data/clean_data/Environmental data/Zonal_stats_all.csv") |> 
-  select(CELL_ID, STD) |> 
-  rename(Cell_ID = CELL_ID)
-
-#import interpolated microclimate indices
-micro_idw <- read.csv("All_data/clean_data/Environmental data/Imke_microclimate_indices_idw_interpolated.csv", row.names = 1)
 
 
 ##Combine SES and environmental data
-comb2 <- env2 |> 
+C5_microenv_comb <- env2 |> 
   #join to microclimate indices |> 
   full_join(micro_idw, by = "Cell_ID") |> 
   #join to remote sensing data |> 
   full_join(rms, by = "Cell_ID") |> 
   #join, one row in env matches many rows in cell_ses due to it containing ses of different traits
-  full_join(cell_ses_poolsite, by = "Cell_ID", relationship = "one-to-many") |>
-  mutate(ncolumn = match(column, LETTERS[1:8]), 
-         grid = paste0(site, grid)) |> #each grid must have a unique id 
-  rename(x_coord = ncolumn, 
-         y_coord = row)
+  full_join(C5_cell_ses_poolsite, by = "Cell_ID", relationship = "one-to-many") |> 
+  mutate(elevation = as.factor(elevation), 
+         grid = as.factor(grid))
 
 
 ###Check collinearity#### 
 library(corrplot)
-cordf <- comb2 |> 
+cordf <- C5_microenv_comb |> 
   filter(trait == "Height_cm") |> #look at just one set of env data, it repeats for every trait
   select(mean_T1_growing_season, mean_moist_growing_season, STD, rock_cover, northness, mean_soil_depth, slope_height) |> 
   drop_na()
@@ -224,20 +202,18 @@ corrplot(cormat, type = "lower", method = "number")
 traitlist <- c("Height_cm","Leaf_area_mm2", "log_LDMC","SLA")
 sitelist <- c("GG", "WH", "BK")
 #lists to store results in
-SES_microenv_summary <- vector(mode= "list", length = length(traitlist) * length(sitelist))
+C5_SES_microenv_summary <- vector(mode= "list", length = length(traitlist) * length(sitelist))
 
-SES_microenv_Rsq<- vector(mode= "list", length = length(traitlist) * length(sitelist))
+C5_SES_microenv_Rsq<- vector(mode= "list", length = length(traitlist) * length(sitelist))
 
-SES_microenv_anova<- vector(mode= "list", length = length(traitlist) * length(sitelist))
+C5_SES_microenv_anova<- vector(mode= "list", length = length(traitlist) * length(sitelist))
 
 l = 1
 for (t in 1:length(traitlist)) {
   for(s in 1:length(sitelist)) {
-  modeldat <-  comb2 |> 
+  modeldat <-  C5_microenv_comb |> 
     filter(trait == traitlist[t], 
            site == sitelist[s]) |> 
-    mutate(elevation = as.factor(elevation), 
-           grid = as.factor(grid)) |> 
     drop_na()
   
   
@@ -298,18 +274,19 @@ for (t in 1:length(traitlist)) {
   sink()
   
   #Also save results in a list object so we can call it with quarto
-  names(SES_microenv_summary)[[l]] <- paste(traitlist[t], sitelist[s], sep = "_")
-  SES_microenv_summary[[l]] <- summary(model)
+  names(C5_SES_microenv_summary)[[l]] <- paste(traitlist[t], sitelist[s], sep = "_")
+  C5_SES_microenv_summary[[l]] <- summary(model)
   
-  names(SES_microenv_Rsq)[[l]] <- paste(traitlist[t], sitelist[s], sep = "_")
-  SES_microenv_Rsq[[l]] <- r.squaredGLMM(model)
+  names(C5_SES_microenv_Rsq)[[l]] <- paste(traitlist[t], sitelist[s], sep = "_")
+  C5_SES_microenv_Rsq[[l]] <- r.squaredGLMM(model)
   
-  names(SES_microenv_anova)[[l]] <- paste(traitlist[t], sitelist[s], sep = "_")
-  SES_microenv_anova[[l]] <- anova(model)
+  names(C5_SES_microenv_anova)[[l]] <- paste(traitlist[t], sitelist[s], sep = "_")
+  C5_SES_microenv_anova[[l]] <- anova(model)
   
   l = l+1
 
-}}
+  }}
+rm(model, modeldat)
 #plot(checkmodel()) returns the following error: Check it out when you have internet
 #Converting missing values (`NA`) into regular values currently not possible for variables of class `NULL`.
 
